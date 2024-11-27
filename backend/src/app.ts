75b33c8ee0b15d5300f76b6483a8bb244ed7733d
Pratik Tomar <pratiksingh067@gmail.com>
2024-11-28 01:24:37 +0530

    Removed morgan import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -8,7 +7,7 @@ import cookieParser from "cookie-parser";
 config();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://chatify-ai.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
